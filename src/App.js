9a5cab5323b86d10a757cc360672ebeed65be94d
AmirhosseinBidokhti <amir.bidokhti99@gmail.com>
2020-07-05 18:31:46 +0430

    Created the base of project with create-react-app and deleted some of the useless files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import HomePage from './homepage.component';
+// . for same directory, .. outer directory
+
+function App() {
+  return (
+    <HomePage/>
+
+  );
+}
+
+export default App;
