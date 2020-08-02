06841b0dc2375e9beccf0cfb7ce13d85bc5d7989
AmirhosseinBidokhti <amir.bidokhti99@gmail.com>
2020-08-02 20:35:06 +0430

    added redux and implemented userReducer and userAction to Header and App components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
@@ -7,22 +8,18 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'; 
+import { setCurrentUser } from './redux/user/user.actions';
 
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    }
-  }
 
   // new method to close subscription when component unmount; we dont want memory leaks in our JS app.
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+
+    const { setCurrentUser } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -30,7 +27,7 @@ class App extends React.Component {
         
         // subscribe; listen to any changes to that data and also get back to first state
         userRef.onSnapshot(snapShot => {
-          this.setState({
+          setCurrentUser({
             currentUser: {
               id: snapShot.id,
               ...snapShot.data()
@@ -41,9 +38,7 @@ class App extends React.Component {
  
         })
       } else {
-        this.setState({
-          currentUser: userAuth //which will be null
-        });
+        setCurrentUser(userAuth);
       }
     
     })
@@ -56,7 +51,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} /> 
+        <Header /> 
         <Switch>
           <Route exact path="/" component={HomePage}/ >
           <Route excat path='/shop' component={ShopPage}/ >
@@ -64,8 +59,13 @@ class App extends React.Component {
   
         </Switch>
       </div>
-    );
+    )
   }
-  
 }
-export default App;
+
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+
+})
+
+export default connect(null, mapDispatchToProps)(App);
